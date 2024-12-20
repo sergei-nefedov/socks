f982e6b8d43e13a6d6f40780dbe0b9e156c6dee6
sergei-nefedov <post.fvyf6@slmail.me>
2024-12-21 01:08:38 +0400

    test: first rest assured teast created

--- src/test/java/pers/nefedov/socks/SocksControllerTests.java
+++ src/test/java/pers/nefedov/socks/SocksControllerTests.java
@@ -1,4 +1,73 @@
 package pers.nefedov.socks;
 
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import pers.nefedov.socks.models.Socks;
+import pers.nefedov.socks.repositories.SocksRepository;
+
+import java.util.List;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.hasSize;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class SocksControllerTests {
+    @LocalServerPort
+    private Integer port;
+
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>(
+            "postgres:16-alpine"
+    );
+
+    @BeforeAll
+    static void beforeAll() {
+        postgres.start();
+    }
+
+    @AfterAll
+    static void afterAll() {
+        postgres.stop();
+    }
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+    }
+
+    @Autowired
+    SocksRepository socksRepository;
+
+    @BeforeEach
+    void setUp() {
+        RestAssured.baseURI = "http://localhost:" + port;
+        socksRepository.deleteAll();
+    }
+    @Test
+    void shouldGetAllCustomers() {
+        List<Socks> socksList = List.of(
+                new Socks(null, "Черный", 87.7, 1000),
+                new Socks(null, "Серый", 50.0, 150)
+        );
+        socksRepository.saveAll(socksList);
+
+        given()
+                .contentType(ContentType.JSON)
+                .when()
+                .get("/api/socks")
+                .then()
+                .statusCode(200)
+                .body(".", hasSize(2));
+    }
 }
