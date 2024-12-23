6111e84af7ce4522a12a2bb6eb9e2981a6718784
sergei-nefedov <post.fvyf6@slmail.me>
2024-12-23 15:40:21 +0400

    ref: documentation

---
+++ README.md
@@ -0,0 +1,15 @@
+<h3>Демонстрационное RESTful приложение.</h3>
+
+Язык реализации: Java 17<br>
+Фреймворк: Spring Boot 2.7<br>
+База данных:  PostgreSQL<br>
+Версия JDK : 17<br>
+Система сборки: Gradle<br>
+Формат файла для загрузки партий: Excel (.xlsx)<br>
+Тестирование: Rest Assured, JUnit, Testcontainers<br>
+Прочее: Mapstruct, Lombok<br>
+ <br> Запуск осуществляется через pers/nefedov/socks/SocksApplication.java. <br>
+Для работы приложения необходимо запустить PostgreSQL в контейнере Docker с помощью compose.yaml
+
+Описание эндпойнтов с примерами запросов реализовано с помощью Swagger, интерфейс которого находится по адресу http://localhost:8080/swagger-ui/index.html. <br><br>
+Файл test.xlsx, используемый для тестирования загрузки партий, располагается в src/test/resources
--- src/main/java/pers/nefedov/socks/controllers/SwaggerSocksController.java
+++ src/main/java/pers/nefedov/socks/controllers/SwaggerSocksController.java
@@ -20,10 +20,11 @@ public interface SwaggerSocksController {
     @ResponseStatus(HttpStatus.CREATED)
     @Operation(
             summary = "Регистрация прихода носков",
-            description = "Параметры: цвет носков, процентное содержание хлопка, количество.\n" +
+            description = "Параметры: цвет носков, процентное содержание хлопка, количество. " +
                     "Увеличивает количество носков на складе. Если на складе уже имелись носки " +
                     "такого же цвета и с таким же содержанием хлопка, новое поступление будет " +
-                    "присоединено к имеющемуся количеству."
+                    "присоединено к имеющемуся количеству с существующим id. Если таких носков не имелось, id будет " +
+                    "присвоен автоматически"
     )
     ResponseEntity<SocksDto> add(@Validated @RequestBody SocksDto socksDto);
 
@@ -32,7 +33,8 @@ public interface SwaggerSocksController {
     @Operation(
             summary = "Регистрация отпуска носков",
             description = "Параметры: цвет носков, процентное содержание хлопка, количество.\n" +
-                    "Уменьшает количество носков на складе, если их хватает."
+                    "Уменьшает количество носков на складе, если их хватает. Уменьшается количество носков такого " +
+                    "же цвета и с таким же содержанием хлопка, переданный id игнорируется."
     )
     ResponseEntity<SocksDto> subtract(@Validated @RequestBody SocksDto socksDto);
 
@@ -56,7 +58,10 @@ public interface SwaggerSocksController {
     @ResponseStatus(HttpStatus.OK)
     @Operation(
             summary = "Обновление данных носков",
-            description = "Позволяет изменить параметры носков (цвет, процент хлопка, количество) по заданному id."
+            description = "Позволяет изменить параметры носков (цвет, процент хлопка, количество) по заданному id. Id" +
+                    " передается через параметр пути, остальные параметры - через тело запроса. Обновляются только " +
+                    "переданные параметры, остальные могут не передаваться. Id, переданный в теле запроса, " +
+                    "игнорируется."
     )
     ResponseEntity<SocksDto> update(@PathVariable @Valid @Positive long id,
                                     @Validated @RequestBody SocksUpdateDto socksUpdateDto);
@@ -65,7 +70,9 @@ public interface SwaggerSocksController {
     @ResponseStatus(HttpStatus.OK)
     @Operation(
             summary = "Прием нескольких партий носков",
-            description = "Принимает Excel файл с партиями носков, содержащими цвет, процентное содержание хлопка и количество."
+            description = "Принимает Excel файл с партиями носков, содержащими цвет, процентное содержание хлопка и " +
+                    "количество. Если на складе уже имелись носки  такого же цвета и с таким же содержанием хлопка, " +
+                    "новое поступление будет присоединено к имеющемуся количеству."
     )
     ResponseEntity<List<SocksDto>> addBatch(@RequestParam("file") MultipartFile file);
 }
